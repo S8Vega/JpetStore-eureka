20ce20f290ad079402e1dba8a3ffd1fe46700058
bsvega <bsvega@bancolombia.com.co>
2021-12-10 07:18:46 -0500

    first commit

---
+++ src/main/java/com/jpet_store/eureka/JpetStoreEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.jpet_store.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetStoreEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpetStoreEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpet_store/eureka/JpetStoreEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpet_store.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpetStoreEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
